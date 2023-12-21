3e859eb22e6ca3cc18dc62d5d993b39af91fb272
cyrielle26 <cyrielleperminjat@gmail.com>
2023-12-21 20:38:32 +0900

    optimixing in progress

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,20 @@
 /** @format */
 
-import { Center } from "@chakra-ui/react"
+import { Center, Container, useMediaQuery } from "@chakra-ui/react"
 import React from "react"
 import { PacmanLoader } from "react-spinners"
 
 export const Loading = () => {
+	const [isLargerThan480] = useMediaQuery("(min-width: 480px)")
+
 	return (
-		<Center pt={"50px"}>
-			<PacmanLoader color='#DD9F64' />
-		</Center>
+		<Container
+			minHeight={"80vh"}
+			minWidth={"100vw"}
+			display={"flex"}
+			alignItems={"center"}
+			justifyContent={"center"}>
+			<PacmanLoader color='#DD9F64' size={isLargerThan480 ? "25px" : "15px"} />
+		</Container>
 	)
 }
--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -29,7 +29,9 @@ export const Detail = () => {
 	const [recipeInfo, setRecipeInfo] = useState()
 	const [isLoaded, setIsLoaded] = useState(true)
 	const [isLoading, setIsLoading] = useState(true)
+	const [isLargerThan900] = useMediaQuery("(min-width: 900px)")
 	const [isLargerThan480] = useMediaQuery("(min-width: 480px)")
+	const [isLargerThan380] = useMediaQuery("(min-width: 380px)")
 
 	const { id } = useParams()
 
@@ -55,13 +57,13 @@ export const Detail = () => {
 	const textStyle = {
 		fontWeight: "bold",
 		color: "#DD9F64",
-		fontSize: "16px",
+		fontSize: isLargerThan380 ? "16px" : "14px",
 	}
 
 	const linkStyle = {
 		color: "#DD9F64",
 		textDecoration: "underline",
-		fontSize: "16px",
+		fontSize: isLargerThan380 ? "16px" : "14px",
 	}
 
 	return (
@@ -71,90 +73,92 @@ export const Detail = () => {
 					<Loading />
 				) : (
 					recipeInfo && (
-						<Container minWidth={"full"}>
-							<Center>
-								<Wrap
-									minWidth='80wv'
-									pt='150px'
-									display='flex'
-									justifyContent='center'>
-									<VStack>
-										<Wrap width='100%'>
-											<Box>
-												<Wrap>
-													<Skeleton
-														height='100%'
-														isLoaded={isLoaded}
-														startColor='#404040'
-														endColor='#505050'
-														fadeDuration={2}>
-														<Image
-															src={
-																recipeInfo.image ||
-																"https://congtygiaphat104.com/template/Default/img/no.png"
-															}
-															alt={recipeInfo.id}
-															objectFit='cover'
-															width='556px'
-															height='370px'
-															onLoad={() => setIsLoaded(true)}
-														/>
-													</Skeleton>
-												</Wrap>
-											</Box>
-											<Box
-												display='flex'
-												flexDirection='column'
-												justifyContent='space-around'
-												alignItems='center'
-												padding='2rem'>
-												<Text
-													fontSize={isLargerThan480 ? "24px" : "18px"}
-													pb={isLargerThan480 ? 0 : "25px"}>
-													{recipeInfo.title}
-												</Text>
+						<Container
+							minWidth={"full"}
+							minHeight={"100vh"}
+							display={"flex"}
+							justifyContent={"center"}
+							alignItems={"center"}
+							flexDirection={isLargerThan900 ? "row" : "column"}>
+							<Box mt={"50px"}>
+								<Wrap>
+									<Skeleton
+										height='100%'
+										isLoaded={isLoaded}
+										startColor='#404040'
+										endColor='#505050'
+										fadeDuration={2}>
+										<Image
+											src={
+												recipeInfo.image ||
+												"https://congtygiaphat104.com/template/Default/img/no.png"
+											}
+											alt={recipeInfo.id}
+											objectFit='cover'
+											width='556px'
+											height='370px'
+											onLoad={() => setIsLoaded(true)}
+										/>
+									</Skeleton>
+								</Wrap>
+							</Box>
+							<Box
+								display='flex'
+								flexDirection='column'
+								justifyContent='space-around'
+								alignItems='center'
+								padding='2rem'>
+								<Text
+									fontSize={
+										isLargerThan480
+											? "24px"
+											: "18px" && isLargerThan380
+											? "18px"
+											: "16px"
+									}>
+									{recipeInfo.title}
+								</Text>
 
-												<Box
-													display='flex'
-													flexDirection='column'
-													alignItems='center'>
-													<VStack
-														display='flex'
-														flexDirection='column'
-														mb='30px'>
-														<Text fontSize={isLargerThan480 ? "16px" : "14px"}>
-															Ready in
-														</Text>
-														<Circle
-															borderRadius={"50%"}
-															bg={"#DD9F64"}
-															size='50px'>
-															{recipeInfo.readyInMinutes}
-														</Circle>
-														<Text fontSize={isLargerThan480 ? "16px" : "14px"}>
-															Minutes
-														</Text>
-													</VStack>
-													<Text fontSize={isLargerThan480 ? "16px" : "14px"}>
-														{" "}
-														Recipe form :{" "}
-														<span style={textStyle}>
-															{recipeInfo.sourceName}
-														</span>
-													</Text>
-													<Link to={recipeInfo.sourceUrl} style={linkStyle}>
-														Go check the recipe here{" "}
-														<Icon>icon={<ChevronRightIcon />}</Icon>
-													</Link>
-												</Box>
-											</Box>
-										</Wrap>
-										{/* <Wrap>
+								<Box
+									display='flex'
+									flexDirection={"column"}
+									alignItems='center'>
+									<VStack
+										display='flex'
+										flexDirection='column'
+										mb='30px'
+										mt={"30px"}>
+										<Text fontSize={isLargerThan480 ? "16px" : "14px"}>
+											Ready in
+										</Text>
+										<Circle
+											borderRadius={"50%"}
+											bg={"#DD9F64"}
+											size={isLargerThan480 ? "50px" : "35px"}>
+											{recipeInfo.readyInMinutes}
+										</Circle>
+										<Text fontSize={isLargerThan480 ? "16px" : "14px"}>
+											Minutes
+										</Text>
+									</VStack>
+									<Text fontSize={isLargerThan480 ? "16px" : "14px"}>
+										{" "}
+										Recipe form :{" "}
+										<span style={textStyle}>{recipeInfo.sourceName}</span>
+									</Text>
+									<Link
+										to={recipeInfo.sourceUrl}
+										style={linkStyle}
+										target='_blank'>
+										Go check the recipe here{" "}
+										<Icon>icon={<ChevronRightIcon />}</Icon>
+									</Link>
+								</Box>
+							</Box>
+
+							{/* <Wrap>
 											<Box bg='pink' width='100%' height='600px'></Box>
 										</Wrap> */}
-									</VStack>
-								</Wrap>
-							</Center>
 						</Container>
 					)
 				)
